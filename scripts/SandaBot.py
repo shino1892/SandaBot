fa7525cd766f7391706abc840c62f1f454b94d34
shino <shioryo0422@gmail.com>
2025-06-23 09:27:01 +0900

    入退室ロジック修正2

--- scripts/SandaBot.py
+++ scripts/SandaBot.py
@@ -24,20 +24,18 @@ def send_notify(message):
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    # 入室
+    # 純粋な入室時のみ
     if before.channel is None and after.channel is not None:
+        # 三田が入室したとき（1人目なら全員通知）
         if member.id == 823778651681193984 and len(after.channel.members) == 1:
             msg = f"@everyone 三田が現れた！"
             send_notify(msg)
-    # チャンネル移動 or 既に入っているチャンネルでの状態変化
-    elif before.channel is not None and after.channel is not None:
-        # チャンネルが変わった場合のみ通知
-        if before.channel != after.channel:
-            if member.id == 918743342374854657 and len(after.channel.members) > 1:
-                msg = f"ゆうなが参戦した！"
-                send_notify(msg)
-            elif member.id == 1200611206382243933 and len(after.channel.members) > 1:
-                msg = f"もっさんが参戦した！"
-                send_notify(msg)
+        # ゆうな or もっさんが入室したとき（2人以上なら参戦通知）
+        elif member.id == 918743342374854657 and len(after.channel.members) > 1:
+            msg = f"ゆうなが参戦した！"
+            send_notify(msg)
+        elif member.id == 1200611206382243933 and len(after.channel.members) > 1:
+            msg = f"もっさんが参戦した！"
+            send_notify(msg)
 
 client.run(TOKEN)
